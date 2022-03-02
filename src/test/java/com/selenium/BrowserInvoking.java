4cedb634d4c35fd13d34c2e449b9bf750f37178f
Naresh Chauhan <62231215+Nareshchauhan-code@users.noreply.github.com>
2022-03-03 00:09:31 +0530

    Initial commit

---
+++ src/test/java/com/selenium/BrowserInvoking.java
@@ -0,0 +1,28 @@
+package com.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class BrowserInvoking {
+
+    @Test
+    void browserInvoking() {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        webDriver.get("https://www.toolsqa.com/");
+
+        System.out.println(webDriver.getCurrentUrl());
+        System.out.println(webDriver.getTitle());
+
+
+    }
+}
